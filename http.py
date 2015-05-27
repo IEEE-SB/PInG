9bb3a1db99a7cd2b02d84a9d8413b1c0b0dad32d
Dimitrios Sapikas <dsapikas@hotmail.com>
2015-05-27 23:24:00 +0300

    simple http example using bottle framework

---
+++ http.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import gpio
+from bottle import *
+
+@route('/out/<pin>/<value>')
+def show(pin,value):
+	pin = gpio.Gpio(pin)
+	pin.direction("out")
+	pin.value(value)
+	pin.unexport
+
+run(host='localhost', port=8080)
